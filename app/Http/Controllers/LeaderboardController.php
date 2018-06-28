a8ebe792a2a06e81e539f66a73d276c24df12bae
Lennart Fischer <lennart.fischer@adolfinum.de>
2018-06-28 17:38:09 +0200

    Added LeaderboardController

---
+++ app/Http/Controllers/LeaderboardController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LeaderboardController extends Controller
+{
+
+    function topUser() {
+
+        $users = User::orderBy('points', 'DESC')->select('id', 'name', 'points')->take(20)->get();
+
+        foreach ($users as $key => $user) {
+
+            $user['rank'] = $key + 1;
+
+        }
+
+        return $users;
+
+    }
+
+    function userRanking() {
+
+        $id = Auth::user()->id;
+
+        $user_ids = User::orderBy('points', 'DESC')->pluck('id')->toArray();
+
+        $rank = array_search($id, $user_ids);
+
+        $user = Auth::user();
+
+        $user['rank'] = $rank + 1;
+
+        return $user;
+
+    }
+
+}
