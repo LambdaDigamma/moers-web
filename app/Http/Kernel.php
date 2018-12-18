6036bfeee2a8c2e347374ec119c1b6a0b3b9bf2c
Lennart Fischer <lennart.fischer@adolfinum.de>
2018-12-19 00:01:17 +0100

    Removed Entrust Admin GUI

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,5 @@ class Kernel extends HttpKernel
         'role' => \Zizaco\Entrust\Middleware\EntrustRole::class,
         'permission' => \Zizaco\Entrust\Middleware\EntrustPermission::class,
         'ability' => \Zizaco\Entrust\Middleware\EntrustAbility::class,
-        'entrust-gui.admin' => \Acoustep\EntrustGui\Http\Middleware\AdminAuth::class,
     ];
 }
