eb4456b172033719f0d552ce3363398612a87f94
lambdadigamma <info@lambdadigamma.com>
2020-02-02 14:38:24 +0100

    Improved Markdown Editor Support

--- resources/js/Shared/PageComponents/Editors/TextEditor.vue
+++ resources/js/Shared/PageComponents/Editors/TextEditor.vue
@@ -10,10 +10,16 @@
             </span>
         </template>
         <template v-slot:default>
-            <TextareaInput
+            <Editor mode="markdown"
+                    :options="editorOptions"
                     v-model="block.data.text"
-                    label="Text"
-                    class="w-full" />
+                    preview-style="vertical">
+
+            </Editor>
+<!--            <TextareaInput-->
+<!--                    v-model="block.data.text"-->
+<!--                    label="Text"-->
+<!--                    class="w-full" />-->
         </template>
     </EditorPreset>
 
@@ -23,15 +29,49 @@
     import Icon from "../../Icon";
     import TextareaInput from "../../TextareaInput";
     import EditorPreset from "./EditorPreset";
+    import Editor from '@toast-ui/vue-editor/src/Editor.vue'
+    import 'tui-editor/dist/tui-editor.css';
+    import 'tui-editor/dist/tui-editor-contents.css';
+    import 'codemirror/lib/codemirror.css';
+
     export default {
         name: "TextEditor",
-        components: {EditorPreset, TextareaInput, Icon},
+        components: {EditorPreset, TextareaInput, Icon, Editor},
         props: {
             block: {
                 type: Object,
                 required: false
             }
-        }
+        },
+        data() {
+            return {
+                editorOptions: {
+                    hideModeSwitch: true,
+                    usageStatistics: false,
+                    language: 'de_DE',
+                    toolbarItems: [
+                        'heading',
+                        'bold',
+                        'italic',
+                        'strike',
+                        'divider',
+                        'hr',
+                        'quote',
+                        'divider',
+                        'ul',
+                        'ol',
+                        'task',
+                        'indent',
+                        'outdent',
+                        'divider',
+                        'table',
+                        'link',
+                        'divider',
+                    ]
+                },
+                editorVisible: true,
+            };
+        },
     }
 </script>
 
