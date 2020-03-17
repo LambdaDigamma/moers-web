cf7c7bdf0be35b595c621b3e738b6bfc3581f443
Lennart Fischer <info@lambdadigamma.com>
2020-03-18 00:46:58 +0100

    Fixed Some Old Foreign Keys to Prepare For User Deletion

---
+++ database/migrations/2020_03_18_002237_make_user_id_nullable_votes_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakeUserIdNullableTableVotes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('votes', function (Blueprint $table) {
+            $table->unsignedInteger('user_id')->nullable()->change();
+        });
+        Schema::table('votes', function (Blueprint $table) {
+            if (DB::getDriverName() !== 'sqlite') {
+                $table->dropForeign('votes_user_id_foreign');
+            }
+        });
+        Schema::table('votes', function(Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('votes', function (Blueprint $table) {
+            if (DB::getDriverName() !== 'sqlite') {
+                $table->dropForeign('votes_user_id_foreign');
+            }
+        });
+        Schema::table('votes', function(Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+        Schema::table('votes', function (Blueprint $table) {
+            $table->unsignedInteger('user_id')->nullable(false)->change();
+        });
+    }
+}
---
+++ database/migrations/2020_03_18_004218_fix_user_id_foreign_student_information_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FixUserIdForeignStudentInformationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('student_information', function (Blueprint $table) {
+            if (DB::getDriverName() !== 'sqlite') {
+                $table->dropForeign('student_information_user_id_foreign');
+            }
+        });
+        Schema::table('student_information', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('student_information', function (Blueprint $table) {
+            if (DB::getDriverName() !== 'sqlite') {
+                $table->dropForeign('student_information_user_id_foreign');
+            }
+        });
+        Schema::table('student_information', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+}
