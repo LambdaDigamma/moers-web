e037129144abd9da2042dafbead82765922c7c81
Lennart Fischer <info@lambdadigamma.com>
2020-03-19 22:10:43 +0100

    Added Login Button in Menu Bar If Not Logged In

--- resources/js/Shared/Layouts/LayoutGeneral.vue
+++ resources/js/Shared/Layouts/LayoutGeneral.vue
@@ -157,6 +157,11 @@
                                 </transition>
                             </div>
                         </div>
+                        <div class="ml-4 flex items-center md:ml-6" v-else>
+                            <inertia-link :href="route('login')" class="text-gray-400 hover:text-gray-500 focus:outline-none focus:shadow-outline focus:text-gray-500">
+                                Anmelden
+                            </inertia-link>
+                        </div>
                     </div>
                 </div>
                 <main class="flex-1 relative z-0 overflow-y-auto py-6 focus:outline-none" tabindex="0" scroll-region>
