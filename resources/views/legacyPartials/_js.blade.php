5400e5d3d17b942e9a66e3f5fd319faf5952b4ad
Lennart Fischer <info@lambdadigamma.com>
2020-03-24 18:12:59 +0100

    Removed Unused Dependencies

--- resources/views/legacyPartials/_js.blade.php
+++
@@ -1,6 +0,0 @@
-<!-- JavaScript -->
-<script src="https://code.jquery.com/jquery-2.1.2.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.4/js/select2.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-
-@yield('scripts')
--- resources/views/legacyPartials/_nav.blade.php
+++
@@ -1,9 +0,0 @@
-<nav class="grey darken-3">
-    <div class="nav-wrapper container">
-        <a href="#" class="brand-logo">Mein Moers</a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-            {{--<li><a href="#">Geschäfte hinzufügen</a></li>--}}
-            {{--<li><a href="#">Mein Moers in den Medien</a></li>--}}
-        </ul>
-    </div>
-</nav>
