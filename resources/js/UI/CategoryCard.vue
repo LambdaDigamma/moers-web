0233ae07dcbe55ad48ae961167cebda41b885b99
Lennart Fischer <github@lambdadigamma.com>
2021-03-04 18:26:53 +0100

    Added some UI

---
+++ resources/js/UI/CategoryCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <li class="col-span-1 flex shadow-sm rounded-md">
+        <div class="flex-shrink-0 flex items-center justify-center w-16 bg-pink-600 text-white text-sm font-medium rounded-l-md">
+            GA
+        </div>
+        <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+            <div class="flex-1 px-4 py-2 text-sm truncate">
+                <a class="text-gray-900 font-medium hover:text-gray-600" href="#">Geschäfte</a>
+                <p class="text-gray-500">über 100</p>
+            </div>
+            <!--																																								<div class="flex-shrink-0 pr-2">-->
+            <!--																																												<button class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">-->
+            <!--																																																<span class="sr-only">Open options</span>-->
+            <!--																																																<svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">-->
+            <!--																																																				<path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />-->
+            <!--																																																</svg>-->
+            <!--																																												</button>-->
+            <!--																																								</div>-->
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+    name: "CategoryCard"
+}
+</script>
+
+<style scoped>
+
+</style>
