0636b540a7a562ae0b7192b0e0069e85c04a6dff
Lennart Fischer <github@lambdadigamma.com>
2025-03-22 18:02:22 +0100

    small fixes

---
+++ Modules/Events/app/Http/Controllers/API/OrganisationEventsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Events\Http\Controllers\API;
+
+use Modules\Events\Http\Controllers\Controller;
+use Modules\Events\Models\Event;
+use Modules\Management\Models\Organisation;
+use Spatie\LaravelData\PaginatedDataCollection;
+
+class OrganisationEventsController extends Controller
+{
+    public function index(Organisation $organisation)
+    {
+        $events = Event::query()
+            ->where('organisation_id', $organisation->id)
+//            ->with(['venues'])
+            ->chronological()
+            ->paginate(20);
+
+//        return PaginatedDataCollection::
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,28 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Modules\Events\Http\Controllers\API\OrganisationEventsController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::group([
+    'middleware' => ['api'],
+], function () {
+
+    // Localization via Accept-Language header
+
+//    Route::get('/v2/organisations/{organisation:slug}/events', [OrganisationEventsController::class, 'index']);
+//    Route::get('/v2/organisations/{organisation:slug}/events/{event:slug}');
+//    Route::get('/v2/organisations/{organisation:slug}/events/{event:slug}/bulk-download');
+//    Route::get('/v2/organisations/{organisation:slug}/posts');
+
+    // Should also return the events at the location (used for the map of the events)
+//    Route::get('/v2/organisations/{organisation:slug}/events/{event:slug}/locations');
+//    Route::get('/v2/locations/{location:slug}');
+
+//    Route::get('/v2/posts/{post:id}');
+//    Route::get('/v2/pages/{page:id}');
+
+});
