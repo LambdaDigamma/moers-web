d960db01005fd4dfe17fbcac1daf959cea1e746d
Lennart Fischer <info@lambdadigamma.com>
2026-01-02 23:48:50 +0100

    chore: add view horizon gate

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,9 +3,9 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
-    \Modules\Events\Providers\EventsServiceProvider::class,
-    \Modules\Waste\Providers\WasteServiceProvider::class,
-    \Modules\Parking\Providers\ParkingServiceProvider::class,
-    \Modules\News\Providers\NewsServiceProvider::class,
-    \Modules\Multimedia\Providers\MultimediaServiceProvider::class,
+    Modules\Events\Providers\EventsServiceProvider::class,
+    Modules\Multimedia\Providers\MultimediaServiceProvider::class,
+    Modules\News\Providers\NewsServiceProvider::class,
+    Modules\Parking\Providers\ParkingServiceProvider::class,
+    Modules\Waste\Providers\WasteServiceProvider::class,
 ];
