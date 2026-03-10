3b0d910505a4870782c7516654b703b34d0f7879
Lennart Fischer <info@lambdadigamma.com>
2026-03-10 11:11:09 +0100

    feat(console): schedule automatic data loading for all modules
    - Added scheduling for events (Moers events and festival events).
    - Added scheduling for news (External posts and Adolfinum news).
    - Added scheduling for multimedia (Radio broadcasts and mediathek feed).
    - Standardized intervals based on legacy application patterns.

--- routes/console.php
+++ routes/console.php
@@ -3,10 +3,29 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
+use Modules\Events\Console\Commands\LoadMoersEvents;
+use Modules\Events\Console\Commands\LoadMoersFestivalEvents;
+use Modules\Multimedia\Console\Commands\LoadMediathekFeed;
+use Modules\Multimedia\Console\Commands\LoadRadio;
+use Modules\News\Console\Commands\ImportAdolfinumNews;
+use Modules\News\Console\Commands\ImportExternalPostsCommand;
 use Modules\Parking\Console\Commands\UpdateParkingAreas;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
+// Parking
 Schedule::command(UpdateParkingAreas::class)->everyFiveMinutes();
+
+// Events
+Schedule::command(LoadMoersEvents::class)->everyFourHours();
+Schedule::command(LoadMoersFestivalEvents::class)->daily();
+
+// News & Posts
+Schedule::command(ImportExternalPostsCommand::class)->hourly();
+Schedule::command(ImportAdolfinumNews::class)->hourly();
+
+// Multimedia
+Schedule::command(LoadRadio::class)->everyFiveMinutes();
+Schedule::command(LoadMediathekFeed::class)->hourly();
